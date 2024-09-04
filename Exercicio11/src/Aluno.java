aa33f8c5dcf3f4fa496ad700165bedd3161496de
henrique-pizzoni <133734395+henrique-pizzoni@users.noreply.github.com>
2024-09-04 09:02:23 -0300

    Exercicio 11 da aula 5

---
+++ Exercicio11/src/Aluno.java
@@ -0,0 +1,72 @@
+public class Aluno {
+    private String nome;
+    private int matricula;
+    private static int nAlunos=0;
+    private int credito;
+    private int debito;
+
+    public Aluno(){
+        nAlunos += 1;
+    }
+
+    public int getnAlunos()
+    {
+        return nAlunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
+    public int getDebito() {
+        return debito;
+    }
+
+    public void setDebito(int debito) {
+        this.debito = debito;
+    }
+
+    public boolean renovaMatricula()
+    {
+        if (confereDebito() > 0)
+            return false;
+
+        else
+            return true;
+    }
+
+    private int confereDebito()
+    {
+        return debito - credito;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", credito=" + credito +
+                ", debito=" + debito +
+                '}';
+    }
+}
---
+++ Exercicio11/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+
+        Aluno a2 = new Aluno();
+
+
+        a1.setNome("Joao");
+        a1.setMatricula(333);
+        a1.setCredito(50);
+        a1.setDebito(60);
+        System.out.println("Aluno " + a1.getNome() + " renovou matricula: " + a1.renovaMatricula());
+
+        a2.setNome("Marcos");
+        a2.setMatricula(555);
+        a2.setCredito(40);
+        a2.setDebito(0);
+        System.out.println("Aluno " + a2.getNome() + " renovou matricula: " + a2.renovaMatricula());
+
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+
+        System.out.println("Nome do aluno: " + a1.getNome() + " Matrícula: " + a1.getMatricula() + " Credito: " + a1.getCredito() + " Debito: " + a1.getDebito());
+
+
+
+        System.out.println("TOTAL DE ALUNOS: " + a1.getnAlunos());
+
+
+    }
+}
